355e997437cc5b0291a9654de77631f12eacc686
Jay Patel <jaykoladia@gmail.com>
2024-12-11 17:28:38 -0500

    update experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,13 +1,13 @@
 export const experiences = [
   {
     id: 1,
-    title: "Junior Front End Developer",
-    company: "Widle Studio",
-    duration: "February 2022 – July 2022",
+    title: "Application Developer Co-op",
+    company: "Evenica - Burlington, ON",
+    duration: "September 2024 - December 2024",
     description: [
-      "Developed modern, interactive websites using React, integrating JavaScript, HTML, CSS, and Tailwind CSS for sleek designs and optimal user experiences.",
-      "Collaborated with clients and team members to gather requirements, fostering effective communication, and delivering high-quality, tested, and secure code.",
-      "Utilized GitHub for version control, ensuring transparency and collaboration within the team while adhering to best practices.",
+      "Designed and developed responsive, user-centric websites using React and SCSS, improving user experience and cross-browser compatibility for clients.",
+      "Built backend services with C# and integrated MS Dynamics 365 Commerce, while utilizing SQL for database management and optimization.",
+      "Collaborated with clients and cross-functional teams to gather requirements, deliver updates, and produce high-quality, secure, and tested code, using Azure DevOps for efficient task management and version control.",
     ],
   },
 ];
